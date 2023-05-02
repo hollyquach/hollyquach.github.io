27f4871dd9bd1980ac8a72d24aa8d99bfc5ab95b
Holly Quach <hollyquach@gmail.com>
2023-05-01 21:59:30 -0700

    Updated main URI & name info

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://hollyquach.github.io/',
+  title: 'HQ.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Holly Quach',
+  role: 'Software Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in/hollyquach/',
+    github: 'https://github.com/hollyquach',
   },
 }
 
@@ -49,23 +49,24 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'Python',
+  'FastAPI',
+  'Django',
+  'JavaScript',
+  'React',
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'PostGreSQL',
   'Git',
+  'Docker',
   'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Agile',
+  'REST API',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'hollyquach@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
