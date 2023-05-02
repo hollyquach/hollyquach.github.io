f105694cfabd11a65d7b3fead489a6c178a065aa
Holly Quach <hollyquach@gmail.com>
2023-05-01 22:22:01 -0700

    Added favicon and resume; Updated footer with attribution

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,12 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/rajshekhar26/cleanfolio'
-      className='link footer__link'
-    >
-      Created By Raj Shekhar
+    <a href='https://github.com/rajshekhar26/cleanfolio' className='link footer__link'>
+        Cleanfolio by Raj Shekhar
+    </a>
+    &nbsp;|&nbsp;
+    <a href="https://www.flaticon.com/free-icons/rubber-duck" className='link footer__link' title="rubber duck icons">
+        Rubber Duck Icon by Talha Dogar - Flaticon
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const about = {
   role: 'Software Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: '%PUBLIC_URL%/Holly-Quach-resume.pdf',
   social: {
     linkedin: 'https://linkedin.com/in/hollyquach/',
     github: 'https://github.com/hollyquach',
